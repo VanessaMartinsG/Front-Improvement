40b03d780b778ea45567324a2e5f1dcc73250f40
VanessaMartinsG <nessa.martinsgomes@gmail.com>
2021-09-23 22:50:06 -0300

    Feat profile screen
    Foi adicionado o modelo inicial que contêm os perfis

---
+++ Dashboard/Dashboard/js/dashboard.js
@@ -0,0 +1,13 @@
+
+let menu = document.querySelector(".left-panel");
+
+
+function showMenu(){
+
+    if(menu.style.display == "none"){
+        menu.style.display = "flex";   
+
+    }else{
+        menu.style.display = "none";
+    } 
+}
